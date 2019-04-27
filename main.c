d0a66c9af9101669bbdd8eb567c11b25389d2608
Tyson3303634 <48035011+Tyson3303634@users.noreply.github.com>
2019-04-27 19:41:23 +1000

    Updated I hope

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
 // Tyson Bennett (c3303634) 
-// Learning to use github
+// Assignment 1
 
 #include<stdio.h>
+#include<string.h>
+
+int main() 
+{
+  char alpha[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, y, z];
+  int 1;
+}
+// this scans input for most common char
+
+// 
+// loop to see how far it is from most comon char in alphabet
