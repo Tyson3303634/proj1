d7e8b0fb6d9622654afa0956d033322dd3d1e09a
Tyson3303634 <48035011+Tyson3303634@users.noreply.github.com>
2019-04-27 19:53:05 +1000

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main()
 {
   char alpha[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, y, z];
   int 1;
+  scan("%f", *********);
 }
 // this scans input for most common char
 
