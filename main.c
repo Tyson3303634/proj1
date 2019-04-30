1f07c9dee3bd2b461ad5ebd0969a81a3ea80904e
Tyson Bennett <c3303634@uon.edu.au>
2019-04-30 12:52:32 +0000

    Improvement from what I was doing I think

--- main.c
+++ main.c
@@ -3,12 +3,26 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 
 int main() 
 {
-  char alpha[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, y, z];
-  int 1;
-  scan("%f", *********);
+    int k;
+    //int m;
+    //char alpha[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, y, z];
+    char inGoing[150];
+    char outGoing[150];
+    
+    printf("Enter message to be encrypted: \n");
+    scanf("%s", &inGoing);
+    printf("Enter a number between -25 and 25 for the rotation key: \n", &k);
+    if (k > 0)
+    {
+        //outGoing == (inGoing + k) * (26%);
+        printf("it worked for pos");
+    }
+    printf("%s", outGoing);
+    return 0;
 }
 // this scans input for most common char
 
