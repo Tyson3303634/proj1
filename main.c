469433c54dfa9e8bb2a47e624187835f50754619
Tyson Bennett <c3303634@uon.edu.au>
2019-04-30 15:04:36 +0000

    Need to submit something before lab

--- main.c
+++ main.c
@@ -2,29 +2,48 @@
 // Assignment 1
 
 #include<stdio.h>
-#include<string.h>
-#include<math.h>
+
 
 int main() 
 {
-    int k;
-    //int m;
-    //char alpha[a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, y, z];
-    char inGoing[150];
-    char outGoing[150];
+    // Line below is the array which defines our leters in the alphabet for the rotation to circulate
+    char alpha[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ,'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char inGoing[25] = "abcdefghijklmnopqrstuvwxyz";     
+    char outGoing[25];               // When this will be printed it will be the encrypted code            
+    int i = 0;                       // Counter for iterations
+    int a;
+    int k = 1;                      // This int is the key to the rotation value
+    char mChar;                      // Character 
+    int m = 0;
     
-    printf("Enter message to be encrypted: \n");
-    scanf("%s", &inGoing);
-    printf("Enter a number between -25 and 25 for the rotation key: \n", &k);
-    if (k > 0)
+    // Initiating for loop where it will read given characters
+    for (i = 0; i < 27; i++)
     {
-        //outGoing == (inGoing + k) * (26%);
-        printf("it worked for pos");
+        mChar = inGoing[i];         // Assigning the given char string to a character
+        m = mChar;                  // Assigning the character to a workable int
+        if (k > 0 && k <= 25)      // First if statement for the math revolving around the positive key value <= 25
+        {
+            a = ((m + k)%26);               // Given math equation to work ASCII
+            outGoing[i] = alpha[a];         // Assigning a to the correct spot of the encrypted message
+            printf("it worked for pos %d: %d  %c \n", i, a, outGoing[i]);
+
+        }
+        else if (k < 0 && k >= (-25))       // This statement runs if the value of the key is < 0 or >= -25
+        {
+            a = ((m - k)%26);                // Given equation once again to work ASCII only this time with a negative key value
+            outGoing[i] = alpha[a];          // Assigning a to the correct spot of the encrypted message
+            printf("it worked for neg %d: %d  %c \n", i, a, outGoing[i]);
+        }
+        else if (k == 0)                            // If the user inputs 0
+        {
+            printf("No encryption to be made.");    // Tells them instead of crashing
+        }
     }
-    printf("%s", outGoing);
+    for (i = 0; i < 27; i++)                
+    {
+        printf("%c", outGoing[i]);
+    }
+    printf("\n Finished!");
+    printf("\n Tadaaaaa!");
     return 0;
 }
-// this scans input for most common char
-
-// 
-// loop to see how far it is from most comon char in alphabet
